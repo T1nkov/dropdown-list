404def386fd6a9f09f042cd423bb9ab73c19dbf2
Dmitry Tinkov <mrfredyon@gmail.com>
2023-10-08 15:38:37 +0300

    HTML_css

---
+++ app.js
@@ -0,0 +1,42 @@
+const select = document.querySelector(".input");
+const input = document.querySelector("input");
+const block = document.querySelector(".block");
+const img = document.querySelector(".image");
+let flag = false;
+
+select.addEventListener("click", function () {
+  if (!flag) {
+    block.style = "visability: visible";
+    img.style = "background-image: url(./curs.svg)";
+
+    flag = true;
+  } else {
+    img.style = "background-image: url(./cursup.svg)";
+    block.style = "visibility: hidden";
+    flag = false;
+  }
+});
+
+block.addEventListener("click", function (event) {
+  input.value = event.target.textContent;
+  block.style = "visibility: hidden";
+  img.style = "background-image: url(./cursup.svg)";
+  flag = false;
+});
+
+select.addEventListener("dblclick", function (event) {
+  input.disabled = false;
+  input.value = "";
+
+});
+
+input.addEventListener('keydown', (event)=>{
+    if(event.key != 'Enter') return
+    else{
+        const p = document.createElement('p')
+        p.textContent = input.value
+        block.appendChild(p)
+        input.value = ''
+
+    }
+})
